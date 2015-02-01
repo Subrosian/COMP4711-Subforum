c343663540ee2220b050c14fa17a8fe3a66cc4d6
Jacob <subrosian@hotmail.com>
2015-01-31 21:23:13 -0800

    initial creation Signed-off-by:Jacob <subrosian@hotmail.com>

---
+++ application/controllers/Guess.php
@@ -0,0 +1,34 @@
+<?php
+
+/**
+ * Our homepage. Show a table of all the author pictures. Clicking on one should show their quote.
+ * Our quotes model has been autoloaded, because we use it everywhere.
+ * 
+ * controllers/Welcome.php
+ *
+ * ------------------------------------------------------------------------
+ */
+class Guess extends Application {
+
+    function __construct() {
+        parent::__construct();
+    }
+
+    //-------------------------------------------------------------
+    //  The normal pages
+    //-------------------------------------------------------------
+
+    function index() {
+        $this->data['pagebody'] = 'justone';    // the view we want shown
+        
+        //get the author and corresponding quote from the Quotes model, to pass on to our view
+        $record = $this->quotes->get(4);
+        $this->data = array_merge($this->data, $record);
+
+        $this->render();
+    }
+
+}
+
+/* End of file Welcome.php */
+/* Location: application/controllers/Welcome.php */
