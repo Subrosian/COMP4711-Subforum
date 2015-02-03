a41ded35c23fe9e71952b9c551fab3144eb600ca
Jacob <subrosian@hotmail.com>
2015-02-03 00:54:27 -0800

    minor template logo edit, removing unnecessary files Signed-off-by:Jacob <subrosian@hotmail.com>

--- application/views/_template.php
+++ application/views/_template.php
@@ -23,7 +23,7 @@ if (!defined('APPPATH'))
         <div class="container">
             <div class="navbar">
                 <div class="navbar-inner">
-                    <a class="brand" href="/"><img src="/assets/images/subforum.png"/></a>
+                    <a class="brand" href="/"><img src="/assets/images/home-logo.png" style="height:32px" /></a>
                     {menubar} </div>
             </div>           
             <div id="content">
