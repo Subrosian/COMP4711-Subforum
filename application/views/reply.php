ea8d63bffcad9451598431513d3f29c8f032e2d2
Jacob <subrosian@hotmail.com>
2015-03-07 11:14:32 -0800

    Progress Signed-off-by:Jacob <subrosian@hotmail.com>

--- application/views/reply.php
+++ application/views/reply.php
@@ -2,7 +2,7 @@
     Reply:<br>
     Username: <input name="username" size="18" value="{given_username}" class="replysubject" style="font-family:Courier new; width:18em;"><br>
     Subject: <input name="subject" size="40" value="Re: {subject}" class="replysubject" style="font-family:Courier new; width:40em;"><br>
-    <textarea name="message" rows="15" cols="50" style="font-family:Courier new; width:50em;">{messagetxt}</textarea><br>
+    <textarea name="message" rows="15" cols="50" style="font-family:Courier new; width:50em;">{messagebox}</textarea><br>
     <input type="submit" value="Submit reply">
     </form><br><br>
 </div>
