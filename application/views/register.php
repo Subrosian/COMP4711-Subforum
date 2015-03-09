15c3085060ab8b20c560225d221ecf8a9f8aad20
Jacob <subrosian@hotmail.com>
2015-03-09 15:11:00 -0700

    Formatting modification. Signed-off-by:Jacob <subrosian@hotmail.com>

--- application/views/register.php
+++ application/views/register.php
@@ -1,7 +1,7 @@
 <div class="registers"><form method="post" action="{submiturl}">
     <div class="{messageclass}">{message}</div>
     Username: <input name="username" size="18" style="font-family:Courier new; width:18em;"><br>
-    Password: <input name="password" type="password" size="18" class="replysubject" style="font-family:Courier new; width:18em;">
+    Password: <input name="password" type="password" size="18" class="replysubject" style="font-family:Courier new; width:18em;"><br>
     <input type="submit" value="Register">
-    </form><br><br>
+    </form>
 </div>
